bc6fd5d3dd6d675a1361cbc58d8555ac1e734af5
Aadil Shaikh <aadilshaikh0118@gmail.com>
2024-06-25 17:29:15 +0530

    services swiper complet.

--- public/js/Swiper.js
+++ public/js/Swiper.js
@@ -1,97 +1,108 @@
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 10,
-    loop: true,
-    speed: 1000,
-    autoplay: {
-        delay: 1000,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        type: "fraction",
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+  spaceBetween: 10,
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
 var swiper = new Swiper(".mySwiper2", {
-    slidesPerView: 1,
-    spaceBetween: 0,
-    loop: true,
-    // autoplay: {
-    //     delay: 2500,
-    //     disableOnInteraction: false,
-    // },
-    pagination: {
-        el: ".swiper-pagination",
-        type: "fraction",
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  // autoplay: {
+  //     delay: 2500,
+  //     disableOnInteraction: false,
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+    768: {
+      slidesPerView: 4,
     },
-    breakpoints: {
-        640: {
-            slidesPerView: 2,
-        },
-        768: {
-            slidesPerView: 4,
-        },
-        1024: {
-            slidesPerView: 5,
-        },
+    1024: {
+      slidesPerView: 5,
     },
+  },
 });
 
 var swiper = new Swiper(".mySwiper3", {
-    slidesPerView: 3,
-    spaceBetween: 10,
-    loop: true,
-    speed: 1200,
-    autoplay: {
-        delay: 0.1,
-        disableOnInteraction: false,
+  slidesPerView: 3,
+  spaceBetween: 10,
+  loop: true,
+  speed: 1200,
+  autoplay: {
+    delay: 0.1,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-    breakpoints: {
-        640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        768: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-        },
-        1024: {
-            slidesPerView: 5,
-            spaceBetween: 50,
-        },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
     },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
 });
 
 var swiper = new Swiper(".mySwiper4", {
-    slidesPerView: 3,
-    spaceBetween: 10,
-    loop: true,
-    autoplay: {
-        delay: 0.1,
-        disableOnInteraction: false,
-        reverseDirection: true,
+  slidesPerView: 3,
+  spaceBetween: 10,
+  loop: true,
+  autoplay: {
+    delay: 0.1,
+    disableOnInteraction: false,
+    reverseDirection: true,
+  },
+  speed: 1200,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-    speed: 1200,
-    breakpoints: {
-        640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        768: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-        },
-        1024: {
-            slidesPerView: 5,
-            spaceBetween: 50,
-        },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
     },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+
+var swiper = new Swiper(".mySwiper5", {
+  slidesPerView: 3,
+  spaceBetween: 40,
+  loop: true,
+//   speed: 1000,
+//   autoplay: {
+//     delay: 1000,
+//     disableOnInteraction: false,
+//   },
 });
--- services_details.php
+++ services_details.php
@@ -1,7 +1,7 @@
 <?php
-    include_once('header.php');
-    $header = new Header();
-    $header->includeHeader('Services Details', './');
+include_once('header.php');
+$header = new Header();
+$header->includeHeader('Services Details', './');
 ?>
 
 <div class="main-body-servicers">
@@ -10,7 +10,7 @@
         <div class="items ">
             <h3 class="title"> Services </h3>
             <ul>
-                <li><a href="#">Home</a></li>   
+                <li><a href="#">Home</a></li>
                 <li><a href="#"> Services</a></li>
             </ul>
         </div>
@@ -20,7 +20,7 @@
         <img src="public/images/services-page/services-details-img.png" alt="">
         <h4>Heading 1</h4>
         <label>
-            <p> 
+            <p>
                 As one precision CNC  & VMC machining services in Vadodra supplier for a wide range of practices across a spectrum of industries,
                 we provide numerous CNC  & VMC machining processes, including CNC  turning, VMC milling, surface grinding, drilling and tapping.
             </p>
@@ -29,26 +29,71 @@
                 especially for small batches of parts and components, which have small slots with high precision dimensions and fine surface finish requirements.
             </p>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
         </label>
     </div>
     <div class="our-services-details">
-        <h6> <i class="ti ti-arrow-up-right"></i> Our Services</h6>
+        <h6> <i class="ti ti-bookmark-filled"></i> Our Services</h6>
         <h2>Services We Offer</h2>
         <div class="swiper-services-details">
-            
+            <div class="swiper mySwiper5">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <img src="public/images/services-page/services-img-01.png" alt="">
+                        <a href="#" class="swiper-service-text">
+                            <p>CNC & VMC Machining Services</p>
+                            <i class="ti ti-arrow-up-right"></i>
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="public/images/services-page/services-img-02.png" alt="">
+                        <a href="#" class="swiper-service-text">
+                            <p>Custom Sheet Metal works</p>
+                            <i class="ti ti-arrow-up-right"></i>
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="public/images/services-page/services-img-03.png" alt="">
+                        <a href="#" class="swiper-service-text">
+                            <p>Precision Machining Services</p>
+                            <i class="ti ti-arrow-up-right"></i>
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="public/images/services-page/services-img-04.png" alt="">
+                        <a href="#" class="swiper-service-text">
+                            <p>Component & engineering Design Services</p>
+                            <i class="ti ti-arrow-up-right"></i>
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="public/images/services-page/services-img-05.png" alt="">
+                        <a href="#" class="swiper-service-text">
+                            <p>Parts Assembly</p>
+                            <i class="ti ti-arrow-up-right"></i>
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="public/images/services-page/services-img-06.png" alt="">
+                        <a href="#" class="swiper-service-text">
+                            <p>Custom Die & Mold Making Service</p>
+                            <i class="ti ti-arrow-up-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-   
+
 </div>
 
-<?php include_once('footer.php');?>
+<?php include_once('footer.php'); ?>
